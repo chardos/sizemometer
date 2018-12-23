9a44e874eb1b74c29b4bc59eddc8c7cb9b750f66
Richard Tan <chardos@gmail.com>
2018-12-23 13:00:59 +1100

    Extracted "add" command

---
+++ src/commands/add.js
@@ -0,0 +1,47 @@
+const fs = require('fs-extra');
+const { log } = require('../wrappers');
+
+module.exports = async () => {
+  const jsonPath = `${process.cwd()}/sizes.json`;
+  const fileName = `${process.cwd()}/README.md`;
+
+  const stats = await fs.stat(fileName);
+  const { size } = stats;
+
+  const commits = await log();
+  const [latestCommit, ...otherCommits] = commits;
+  const [commitHash, commitMessage, timeAgo, author] = latestCommit;
+
+  const jsonExists = await fs.exists(jsonPath);
+  const existingFile = jsonExists && await fs.readFile(jsonPath);
+  const storedCommits = jsonExists && JSON.parse(existingFile.toString());
+  const lastStoredHash = jsonExists && storedCommits[0].commitHash;
+  const hashIsNew = lastStoredHash !== commitHash;
+
+  if (jsonExists && hashIsNew) {
+    const newArr = [
+      {
+        author,
+        commitHash,
+        commitMessage,
+        size
+      },
+      ...storedCommits
+    ]
+    const formattedArr = JSON.stringify(newArr, null, 2);
+    fs.writeFile(jsonPath, formattedArr);
+    console.log('Added new data point to sizes.json')
+  } else if (!jsonExists) {
+    const newArr = [{
+      author,
+      commitHash,
+      commitMessage,
+      size
+    }]
+    const formattedArr = JSON.stringify(newArr, null, 2);
+    fs.writeFile(jsonPath, formattedArr)
+    console.log('sizes.json not found. Writing new file.')
+  } else {
+    console.log('No new commits to add to sizes.json')
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
-const fs = require('fs-extra');
-const { log } = require('./wrappers');
-
 module.exports = async () => {
-  const jsonPath = `${process.cwd()}/sizes.json`;
-  const fileName = `${process.cwd()}/README.md`;
-
-  const stats = await fs.stat(fileName);
-  const { size } = stats;
-
-  const commits = await log();
-  const [latestCommit, ...otherCommits] = commits;
-  const [commitHash, commitMessage, timeAgo, author] = latestCommit;
-
-  const jsonExists = await fs.exists(jsonPath);
-  const existingFile = jsonExists && await fs.readFile(jsonPath);
-  const storedCommits = jsonExists && JSON.parse(existingFile.toString());
-  const lastStoredHash = jsonExists && storedCommits[0].commitHash;
-  const hashIsNew = lastStoredHash !== commitHash;
-
-  if (jsonExists && hashIsNew) {
-    const newArr = [
-      {
-        author,
-        commitHash,
-        commitMessage,
-        size
-      },
-      ...storedCommits
-    ]
-    const formattedArr = JSON.stringify(newArr, null, 2);
-    fs.writeFile(jsonPath, formattedArr);
-  } else if (!jsonExists) {
-    const newArr = [{
-      author,
-      commitHash,
-      commitMessage,
-      size
-    }]
-    const formattedArr = JSON.stringify(newArr, null, 2);
-    fs.writeFile(jsonPath, formattedArr)
-  }
+  const command = process.argv[2];
+  const script = require(`./commands/${command}.js`)
+  script();
 };
