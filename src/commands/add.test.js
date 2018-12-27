dbdee9a57bf376092ad579f4c3734f445529d74a
Richard Tan <chardos@gmail.com>
2018-12-27 18:16:11 +1100

    Add test dependencies

---
+++ src/commands/add.test.js
@@ -0,0 +1,5 @@
+describe('add command', () => {
+  it('should', () => {
+    
+  })
+})
--- src/wrappers.js
+++ src/wrappers.js
@@ -1,4 +1,7 @@
 const git = require('git-rev')
+const cmd = require('node-cmd');
+const { promisify } = require('es6-promisify');
+const asyncCmd = promisify(cmd.get);
 
 exports.log = () => new Promise((resolve) => {
   git.log(function (array) {
