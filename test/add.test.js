9c56a9f18d65a3135760e0644f537aea14600f9e
Richard Tan <chardos@gmail.com>
2018-12-28 21:49:54 +1100

    Added assertions

--- test/add.test.js
+++ test/add.test.js
@@ -1,7 +1,9 @@
+const fs = require('fs-extra');
 const { addConfigFile, writeFile } = require('./helpers');
 const { cmd } = require('./wrappers');
 const nodeCmd = require('node-cmd');
 const { largeText, smallText } = require('./dummys/textFiles');
+const { SIZES_JSON_PATH } = require('./constants');
 
 // Test:
 // no sizes.json
@@ -17,13 +19,21 @@ describe('Command: Add', () => {
         body: smallText
       });
 
-      await cmd('sizemometer add').then((output, a, b) => {
+      await cmd('sizemometer add').then((output) => {
         console.log('output:', output);
       });
 
+      const jsonExists = await fs.exists(SIZES_JSON_PATH);
+      expect(jsonExists).toEqual(true);
 
-      // assert file exists
-      // assert a snapshot
+      const sizesJsonBuffer = await fs.readFile(SIZES_JSON_PATH);
+      expect(
+        sizesJsonBuffer.toString()
+      ).toMatchSnapshot()
     })
+  })
+
+  describe('when there is a pre-existing sizes.json', () => {
+    
   })
 })
--- test/constants.js
+++ test/constants.js
@@ -1,7 +1,9 @@
 const TEST_DIRECTORY = `${process.cwd()}/tmp`;
+const SIZES_JSON_PATH = `${TEST_DIRECTORY}/sizes.json`;
 const CONFIG_PATH = `${TEST_DIRECTORY}/sizemometer.config.js`;
 
 module.exports = {
   TEST_DIRECTORY,
+  SIZES_JSON_PATH,
   CONFIG_PATH
 }
