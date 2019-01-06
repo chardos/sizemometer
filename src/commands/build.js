0c08677e3828802a7bff6df0e6330ba7040ddd03
Richard Tan <chardos@gmail.com>
2019-01-06 23:17:39 +1100

    Using chart.js

--- src/commands/build.js
+++ src/commands/build.js
@@ -4,15 +4,13 @@ const fs = require('fs-extra');
 console.log('config', config);
 
 module.exports = async () => {
-  console.log('building');
-
   // copy history.json
   const historyOriginPath = `${process.cwd()}/.sizemometer/history.json`;
   const historyDestinationPath = `${__dirname}/../../template/history.json`;
 
   await fs.copyFile(historyOriginPath, historyDestinationPath, (err) => {
     if (err) throw err;
-    console.log('source.txt was copied to destination.txt');
+    console.log(`${historyOriginPath} was copied to ${historyDestinationPath}`);
   });
 
   const compiler = webpack(config);
--- template/components/App.js
+++ template/components/App.js
@@ -1,8 +1,26 @@
 const history = require('../history.json');
 import React from 'react';
+import BarChartWrapper from './BarChartWrapper';
 
 console.log('history', history);
 
-const App = () => <div>This is an app mate</div>;
+const App = () => {
+  const fileHistories = Object.keys(history).map(key => {
+    return {
+      filename: key,
+      entries: history[key]
+    }
+  })
+
+  return (
+    <div>
+      {
+        fileHistories.map(fileHistoryItem => (
+          <BarChartWrapper fileHistoryItem={fileHistoryItem} />
+        ))
+      }
+    </div>
+  );
+};
 
 export default App;
