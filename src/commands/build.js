eb27dc91452245d9bc6fd51051aeb66a5dc60b7f
Richard Tan <chardos@gmail.com>
2019-01-12 16:07:59 +1100

    Copying index file to /report

--- src/commands/build.js
+++ src/commands/build.js
@@ -3,13 +3,22 @@ const config = require('../../template/webpack.config');
 const fs = require('fs-extra');
 
 module.exports = async () => {
-  // copy history.json
+  // copy history.json into node_modules for the build
   const historyOriginPath = `${process.cwd()}/.sizemometer/history.json`;
   const historyDestinationPath = `${__dirname}/../../template/history.json`;
 
   await fs.copyFile(historyOriginPath, historyDestinationPath, (err) => {
     if (err) throw err;
     console.log(`${historyOriginPath} was copied to ${historyDestinationPath}`);
+  });
+
+  // copy index.html over to report
+  const indexOriginPath = `${__dirname}/../../template/index.html`;
+  const indexDestinationPath = `${process.cwd()}/.sizemometer/report/index.html`;
+
+  await fs.copyFile(indexOriginPath, indexDestinationPath, (err) => {
+    if (err) throw err;
+    console.log(`${indexOriginPath} was copied to ${indexDestinationPath}`);
   });
 
   const compiler = webpack(config);
--- src/commands/report.js
+++ src/commands/report.js
@@ -1,6 +1,9 @@
+const fs = require('fs-extra');
 const opn = require('opn');
 
-module.exports =  () => {
+module.exports =  async() => {
   const reportPath = `${process.cwd()}/.sizemometer/report/index.html`;
+  const reportExists = await fs.exists(reportPath);
+  if (!reportExists) { throw new Error('No report exists. Please run "sizemometer build"') }
   opn(reportPath)
 }
