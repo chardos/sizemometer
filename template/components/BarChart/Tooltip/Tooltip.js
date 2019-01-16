93e12ce3e87474aac4cfabe453decef2789aa050
Richard Tan <chardos@gmail.com>
2019-01-16 19:47:21 +1100

    Arrow on tooltip

--- template/components/BarChart/Tooltip/Tooltip.js
+++ template/components/BarChart/Tooltip/Tooltip.js
@@ -7,17 +7,32 @@ export const TooltipWrapper = styled.div`
   text-align: center;
   opacity: 0;
   position: absolute;
-  top: -40px;
+  top: -70px;
   left: 50%;
-  transform: translate(-50%, -4px);
+  transform: translate(-50%, 4px);
   width: 90px;
   padding: 12px 8px;
   border-radius: 10px;
-  background-color: rgba(33,47,58,90);
+  background-color: rgba(33,47,58,0.9);
   color: #dadada;
   z-index: 1;
   font-size: 12px;
   transition: 0.2s all;
+
+  &:after {
+    top: 100%;
+    left: 50%;
+    border: solid transparent;
+    content: " ";
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+    border-color: transparent;
+    border-top-color: rgba(33,47,58,0.9);
+    border-width: 8px;
+    margin-left: -8px;
+  }
 `
 
 const Tooltip = ({data, children}) => {
