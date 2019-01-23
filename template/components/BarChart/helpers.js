9e4b7bb639c99ad7d5c242f77fabab4e4144ef6d
Richard Tan <chardos@gmail.com>
2019-01-23 23:51:05 +1100

    getPercentageFromRange() working

--- template/components/BarChart/helpers.js
+++ template/components/BarChart/helpers.js
@@ -25,8 +25,16 @@ export const prepareData = (data, panelWidth) => {
   }
 }
 
-export function getPercentage({minValue, maxValue, currentValue, offset}) {
-  
+export function getPercentageFromRange({minValue, maxValue, currentValue, offsetBottom = 0}) {
+  const range = maxValue - minValue;
+  const adjustedValue = currentValue - minValue;
+  const percentage = adjustedValue / range * 100;
+  return scaleMin({percentage, minimum: offsetBottom});
+}
+
+// Scales a percentage up based on a minimum value
+export function scaleMin({percentage, minimum}) {
+  return percentage + (minimum * (100 - percentage)) / 100;
 }
 
 function addEmptyBars(numBars) {
--- template/components/BarChart/helpers.test.js
+++ template/components/BarChart/helpers.test.js
@@ -1,37 +1,75 @@
-import { getPercentage } from './helpers';
+import { getPercentageFromRange, scaleMin } from './helpers';
 
-describe('getPercentage', () => {
-  it('should return the correct percentage', () => {
-    const testCases = [{
-      minValue: 150,
-      maxValue: 300,
-      currentValue: 150,
-      offset: 0,
-      expectedResult: 0,
-    }]
+describe('getPercentageFromRange', () => {
+  
+    const testCases = [
+      {
+        minValue: 150,
+        maxValue: 300,
+        currentValue: 150,
+        offsetBottom: 0,
+        expectedResult: 0,
+      },
+      {
+        minValue: 150,
+        maxValue: 300,
+        currentValue: 225,
+        offsetBottom: 0,
+        expectedResult: 50,
+      },
+      {
+        minValue: 150,
+        maxValue: 300,
+        currentValue: 150,
+        offsetBottom: 10,
+        expectedResult: 10,
+      },
+      {
+        minValue: 150,
+        maxValue: 300,
+        currentValue: 225,
+        offsetBottom: 0,
+        expectedResult: 50,
+      },
+    ]
 
     testCases.forEach((testCase) => {
-      const result = getPercentage(testCase);
-      expect(result).toEqual(testCase.expectedResult);
+      const {minValue, maxValue, currentValue, offsetBottom, expectedResult} = testCase;
+      const result = getPercentageFromRange(testCase);
+      it(`maxValue: ${maxValue}, minValue: ${minValue}, currentValue: ${currentValue}, offsetBottom: ${offsetBottom}, should return ${expectedResult}`, () => {
+        expect(result).toEqual(testCase.expectedResult);
+      })
     })
-
-    // getPercentage({minValue, maxValue, currentValue, offset})
-  })
 })
 
+describe('scaleMin', () => {
+  it('should return the correct percentage', () => {
+    const testCases = [
+      {
+        percentage: 0,
+        minimum: 10,
+        expectedResult: 10,
+      },
+      {
+        percentage: 50,
+        minimum: 10,
+        expectedResult: 55,
+      },
+      {
+        percentage: 100,
+        minimum: 10,
+        expectedResult: 100,
+      },
+      {
+        percentage: 50,
+        minimum: 20,
+        expectedResult: 60,
+      },
+    ]
 
-// 300 - 100%
-// 225 - 55%
-// 150 - 10%
-
-// maxValue 300
-// minValue 150
-
-
-// range = maxValue - minValue = 150
-
-// 300 - minValue = 150
-
-// 225 - minValue = 75
-
-// 150 - minValue = 0;
+    testCases.forEach((testCase) => {
+      const result = scaleMin(testCase);
+      expect(result).toEqual(testCase.expectedResult);
+    })
+  })
+})
