0ce514f3a62d384b5cef135390c4db9baeba0b01
Richard Tan <chardos@gmail.com>
2019-01-28 14:03:08 +1100

    Bump again

--- src/pipelines/add/addGitData.js
+++ src/pipelines/add/addGitData.js
@@ -3,8 +3,9 @@ const git = require('get-git-data');
 module.exports = async (data) => {
   const { files } = data;
   const commits = await git.log();
+
   const latestCommit = commits[0];
-  const [commitHash, commitMessage, timestamp, author] = latestCommit;
+  const {commitHash, commitMessage, timestamp, authorName: author} = latestCommit;
 
   const newFiles = files.map(file => ({
     ...file,
