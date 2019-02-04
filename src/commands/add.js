60225fba1fc2f7c061f4fbba5e4c55bacdb051a4
Richard Tan <chardos@gmail.com>
2019-02-04 22:04:43 +1100

    add.js fully refactored

--- src/commands/add.js
+++ src/commands/add.js
@@ -5,7 +5,7 @@ const updateHistoryJson = require('../pipelines/add/updateHistoryJson');
 const buildHistoryJsonP = require('../pipelines/add/buildHistoryJsonP');
 const buildConfigJsonP = require('../pipelines/add/buildConfigJsonP');
 const getTrackedFiles = require('../pipelines/add/getTrackedFiles');
-const setup = require('../pipelines/add/setup');
+// const setup = require('../pipelines/add/setup');
 const getPaths = require('../utils/getPaths');
 
 module.exports = async (
@@ -13,18 +13,11 @@ module.exports = async (
   scopedPath, // path scoping for tests
 ) => {
   const paths = getPaths(scopedPath);
-  const trackedFiles = getTrackedFiles(paths, scopedPath);
-
-  await setup(scopedPath)
-    .then(addFileSizes)
-    .then(async (data) => {
-      const filesWithGitData = await injectedAddGitData(data);
-      const historyJson = await getHistoryJson(paths);
-      await updateHistoryJson(paths, historyJson, filesWithGitData)
-      await buildHistoryJsonP(paths)
-      await buildConfigJsonP(paths)
-    })
-    // .catch((err) => {
-    //   throw new Error(err);
-    // });
+  const trackedFiles = await getTrackedFiles(paths, scopedPath);
+  const filesWithSizes = await addFileSizes(trackedFiles);
+  const filesWithGitData = await injectedAddGitData(filesWithSizes);
+  const historyJson = await getHistoryJson(paths);
+  await updateHistoryJson(paths, historyJson, filesWithGitData)
+  await buildHistoryJsonP(paths)
+  await buildConfigJsonP(paths)
 };
