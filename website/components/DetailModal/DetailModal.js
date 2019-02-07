a281596f88a6d46d432a0a71b0e616e787540c04
Richard Tan <chardos@gmail.com>
2019-02-07 23:20:40 +1100

    Add black background behind modal

--- website/components/DetailModal/DetailModal.js
+++ website/components/DetailModal/DetailModal.js
@@ -18,43 +18,45 @@ const DetailModal = ({ detailModal, histories, closeModal }) => {
   const reversedData = [...history.data].reverse();
 
   return (
-    <s.DetailModalWrapper>
-      <s.Title>{history.filename}</s.Title>
-      <s.CloseButton onClick={closeModal}>X</s.CloseButton>
+    <s.BlackOverlay>
+      <s.DetailModalWrapper>
+        <s.Title>{history.filename}</s.Title>
+        <s.CloseButton onClick={closeModal}>X</s.CloseButton>
 
-      <s.Grid>
-        <s.GridTitle>Author</s.GridTitle>
-        <s.GridTitle>Commit hash</s.GridTitle>
-        <s.GridTitle>Commit message</s.GridTitle>
-        <s.GridTitle>Size</s.GridTitle>
-        <s.GridTitle />
+        <s.Grid>
+          <s.GridTitle>Author</s.GridTitle>
+          <s.GridTitle>Commit hash</s.GridTitle>
+          <s.GridTitle>Commit message</s.GridTitle>
+          <s.GridTitle>Size</s.GridTitle>
+          <s.GridTitle />
 
-        {reversedData.map(({
-          commitHash, author, commitMessage, size,
-        }) => {
-          const percentage = getPercentageFromRange({
-            minValue,
-            maxValue,
-            currentValue: size,
-            offsetBottom: 10
-          });
+          {reversedData.map(({
+            commitHash, author, commitMessage, size,
+          }) => {
+            const percentage = getPercentageFromRange({
+              minValue,
+              maxValue,
+              currentValue: size,
+              offsetBottom: 10
+            });
 
-          const GridItem = commitHash === detailModal.commitHash
-            ? s.HighlightedGridItem
-            : s.GridItem;
+            const GridItem = commitHash === detailModal.commitHash
+              ? s.HighlightedGridItem
+              : s.GridItem;
 
-          return (
-            <React.Fragment key={commitHash}>
-              <GridItem>{author}</GridItem>
-              <GridItem>{shortCommitHash(commitHash)}</GridItem>
-              <GridItem>{commitMessage}</GridItem>
-              <GridItem>{filesize(size)}</GridItem>
-              <GridItem><HorizontalBar percentage={percentage} /></GridItem>
-            </React.Fragment>
-          );
-        })}
-      </s.Grid>
-    </s.DetailModalWrapper>
+            return (
+              <React.Fragment key={commitHash}>
+                <GridItem>{author}</GridItem>
+                <GridItem>{shortCommitHash(commitHash)}</GridItem>
+                <GridItem>{commitMessage}</GridItem>
+                <GridItem>{filesize(size)}</GridItem>
+                <GridItem><HorizontalBar percentage={percentage} /></GridItem>
+              </React.Fragment>
+            );
+          })}
+        </s.Grid>
+      </s.DetailModalWrapper>
+    </s.BlackOverlay>
   );
 };
 
