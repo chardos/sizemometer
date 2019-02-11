912dcaee0b173a863307a70e81374f81af7b97fb
Richard Tan <chardos@gmail.com>
2019-02-11 21:47:53 +1100

    Remove unused helper

--- website/components/BarChart/helpers.js
+++ website/components/BarChart/helpers.js
@@ -32,25 +32,3 @@ export const prepareData = (data, panelWidth) => {
     bars: preparedBars,
   };
 };
-
-// Scales a percentage up based on a minimum size
-export function scaleMin({ percentage, minimum }) {
-  return percentage + (minimum * (100 - percentage)) / 100;
-}
-
-export function getPercentageFromRange({
-  minValue, maxValue, currentValue, offsetBottom = 0,
-}) {
-  const range = maxValue - minValue;
-  console.log('range', range);
-
-  // if there is only 1 value, range is 0, which will bring
-  // the calculation. Just return 50% if theres only 1 value.
-  if (range === 0) {
-    return 50;
-  }
-
-  const adjustedValue = currentValue - minValue;
-  const percentage = adjustedValue / range * 100;
-  return scaleMin({ percentage, minimum: offsetBottom });
-}
